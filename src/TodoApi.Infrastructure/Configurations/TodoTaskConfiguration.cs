008105f8669c93d7d01f3f80ea0028f599f52cf3
JayLog22 <jay.log22@gmail.com>
2025-10-21 13:40:10 -0300

    base repository pattern

---
+++ src/TodoApi.Infrastructure/Configurations/TodoTaskConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoApi.Core.Entities;
+using TodoApi.Core.Entities.Enums;
+
+namespace TodoApi.Infrastructure.Configurations;
+
+public class TodoTaskConfiguration : IEntityTypeConfiguration<TodoTask>
+{
+
+    public void Configure(EntityTypeBuilder<TodoTask> builder)
+    {
+        builder.ToTable("TodoTasks");
+
+        #region Keys and indexes
+
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.IsCompleted);
+        builder.HasIndex(x => x.Priority);
+        builder.HasIndex(x => x.CreatedAt);
+        
+        #endregion
+
+        #region Entity base properties
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("GETUTCDATE()");
+
+        builder.Property(x => x.UpdatedAt);
+        
+        #endregion
+        
+        #region Properties
+        
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(1000);
+
+        builder.Property(x => x.IsCompleted)
+            .IsRequired()
+            .HasDefaultValue(false);
+
+        builder.Property(x => x.DueDate);
+
+        builder.Property(x => x.Priority)
+            .IsRequired()
+            .HasDefaultValue(Priority.Medium);
+
+        builder.Property(x => x.Tags)
+            .HasMaxLength(500);
+        
+        #endregion
+    }
+}
